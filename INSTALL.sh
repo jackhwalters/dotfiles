c39363e1bb7889e07f0d4e45aa49d4948d9f07a3
Jack Walters <jackwalterswork@gmail.com>
2023-08-05 12:48:28 +0100

    Rm update script and symlink all files in install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,29 +1,47 @@
+#! /usr/bin/bash
+
+# Install Vundle and symlink color scheme
 if [ ! -d "../.vim/bundle/Vundle.vim"  ]
 then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 
 if [ -d "../.vim/colors"  ]
 then
-    cp onedark.vim ../.vim/colors/onedark.vim
+    ln -s $HOME/dotfiles/onedark.vim $HOME/.vim/colors/onedark.vim
 else
     mkdir ../.vim/colors/
-    cp onedark.vim ../.vim/colors/onedark.vim
+    ln -s $HOME/dotfiles/onedark.vim $HOME/.vim/colors/onedark.vim
 fi
 
 if [ -d "../.vim/autoload"  ]
 then
-    cp onedark.vim ../.vim/autoload/onedark.vim
+    ln -s $HOME/dotfiles/onedark.vim $HOME/.vim/autoload/onedark.vim
 else
     mkdir ../.vim/autoload/
-    cp onedark.vim ../.vim/autoload/onedark.vim
+    ln -s $HOME/dotfiles/onedark.vim $HOME/.vim/autoload/onedark.vim
 fi
 
-cp .vimrc ../.vimrc
-cp .bashrc ../.bashrc
-cp .gitconfig ../.gitconfig
-cp .tmux.conf ../.tmux.conf
-cp .zshrc ../.zshrc
-cp .aliases ../.aliases
+# Symlink RCs
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.aliases $HOME/.aliases
+
+# Symlink VS Code configs
+if [[ $(uname -s) == 'Darwin'* ]]
+then
+    ln -s $HOME/dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+    ln -s $HOME/dotfiles/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+elif [[ $(uname -s) == 'Linux'* ]]
+then
+    ln -s $HOME/dotfiles/settings.json $HOME/.config/Code/User/settings.json
+    ln -s $HOME/dotfiles/keybindings.json $HOME/.config/Code/User/keybindings.json
+else
+    echo "This script is only configured for Mac and Linux"
+    exit 1
+fi
 
 exit 0
