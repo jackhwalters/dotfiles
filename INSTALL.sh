fc6a546a59e53947ac001d8d1106884fd0adab5d
Jack Walters <jackwalterswork@gmail.com>
2023-10-27 23:05:19 +0100

    Add git clone p10k to INSTALL

--- INSTALL.sh
+++ INSTALL.sh
@@ -33,6 +33,8 @@ else
     exit 1
 fi
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 exit 0
