4d1bb5659f89064237a778c9e89687bff6dcc29c
Jack Walters <jackwalterswork@gmail.com>
2024-01-31 11:23:10 +0000

    Add Win installation steps in INSTALL1, and reindent .gitconfig

--- INSTALL1.sh
+++ INSTALL1.sh
@@ -1,7 +1,16 @@
 #! /usr/bin/bash
 
 # Install zsh
-sudo apt install zsh
+if [[ $(uname -s) == 'Darwin'* || "$(expr substr $(uname -s) 1 5)" == "Linux" ]]
+then
+    sudo apt install zsh
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" || "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]
+then
+    pacman -S zsh
+else
+    echo "Unrecognised OS for installing ZSH"
+fi
+
 
 # Symlink RCs
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
@@ -24,14 +33,20 @@ then
     ln -s $HOME/dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
     mkdir -p $HOME/Library/Application\ Support/Code/User/
     ln -s $HOME/dotfiles/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-elif [[ $(uname -s) == 'Linux'* ]]
+elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]
 then
     mkdir -p $HOME/.config/Code/User/
     ln -s $HOME/dotfiles/settings.json $HOME/.config/Code/User/settings.json
     ln -s $HOME/dotfiles/keybindings.json $HOME/.config/Code/User/keybindings.json
+elif [[ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" || "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]
+then
+    mkdir -p $HOME/AppData/Roaming/Code/User
+    ln -s $HOME/dotfiles/settings.json $HOME/AppData/Roaming/Code/User/settings.json
+    ln -s $HOME/dotfiles/keybindings.json $HOME/AppData/Roaming/Code/User/keybindings.json
 else
     echo "This script is only configured for Mac and Linux"
-    exit 1
 fi
 
+rm -rf $HOME/.oh-my-zsh/
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
