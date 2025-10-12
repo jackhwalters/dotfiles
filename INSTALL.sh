861d9794313a7584eebee7177a7ff735973423c2
Jack Walters <jackwalterswork@gmail.com>
2025-10-12 19:58:38 +0100

    Add cursor settings

--- INSTALL.sh
+++ INSTALL.sh
@@ -11,6 +11,7 @@ ln -s $HOME/dotfiles/.aliases $HOME/.aliases
 ln -s $HOME/dotfiles/.p10k.zsh $HOME/.p10k.zsh
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 rm -f ~/Library/Application\ Support/Code/User/settings.json && ln -s $HOME/dotfiles/vscode-settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+rm -f ~/Library/Application\ Support/Cursor/User/settings.json && ln -s $HOME/dotfiles/cursor-settings.json "$HOME/Library/Application Support/Cursor/User/settings.json"
 
 # Install Neovim and tmux
 if [[ $(uname -s) == 'Darwin'* ]]
