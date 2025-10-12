5b7287b64bededb608adc1dc00392a85bbd3672b
Jack Walters <jackwalterswork@gmail.com>
2025-10-12 19:34:45 +0100

    Update INSTALL

--- INSTALL.sh
+++ INSTALL.sh
@@ -48,7 +48,7 @@ else
 fi
 
 # Install PowerLevel10k
+rm -rf ~/powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 source $HOME/.zshrc
